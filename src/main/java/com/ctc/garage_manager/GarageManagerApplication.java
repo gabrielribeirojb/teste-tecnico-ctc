4c618b7660dca15326eeb4f17bc5667f859a7bb8
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2025-05-20 20:57:56 -0300

    Projeto criado com Spring Boot 3.4.5 e Java 21

---
+++ src/main/java/com/ctc/garage_manager/GarageManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ctc.garage_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageManagerApplication.class, args);
+	}
+
+}
